35d92019afa29eb92c5bdb77b032ccbdea707960
dyn64 <dynamical@gmail.com>
2026-06-21 17:28:34 +0200

    2.1 fixed

--- src/textnode.py
+++ src/textnode.py
@@ -9,16 +9,21 @@ class TextType(Enum):
     IMAGE = "image"
 
 class TextNode():
-    def __init__(self, txt, txt_type, url):
+    def __init__(self, txt: str, txt_type: TextType, url: str) -> None:
         self.text = txt
         self.text_type = txt_type
         self.url = url
 
-    def __eq__(self, value):
+    def __eq__(self, other: "TextNode") -> bool:
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
         #compares self to another TextNode instance (other) and returns True if all of their properties are equal. 
         pass
 
     def __repr__(self):
         #returns a string representation of the TextNode object.
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
         
