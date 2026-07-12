fc54d585591a55e18ecf74200531ade41390ce9d
dyn64 <dynamical@gmail.com>
2026-07-12 14:54:24 +0200

    3.3

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,25 @@
+import unittest
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_image(self):
+        markdown_text = 'text text and ![alt text](https://nerd.net/test.png) text'
+        correct_links = [("alt text", "https://nerd.net/test.png")]
+        self.assertListEqual(extract_markdown_images(markdown_text), correct_links)
+
+    def test_link(self):
+        markdown_text = 'text text and [anchor text](https://nerd.net) text'
+        correct_links = [("anchor text", "https://nerd.net")]
+        self.assertListEqual(extract_markdown_links(markdown_text), correct_links)
+
+    def test_link_multiple(self):
+        markdown_text = 'text [anchor 1](https://slashdot.org) text and [anchor text](https://nerd.net) text'
+        correct_links = [
+            ("anchor 1", "https://slashdot.org"),
+            ("anchor text", "https://nerd.net")
+            ]
+        self.assertListEqual(extract_markdown_links(markdown_text), correct_links)
+
+
+if __name__ == "__main__":
+    unittest.main()
