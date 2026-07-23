253cf84ad8731a0f1e61f27dac993454b212d5a0
dyn64 <dynamical@gmail.com>
2026-07-23 13:19:41 +0200

    5.3 .

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
-# herro wrod
-
 from textnode import *
 from copy_files import update_public_dir
 from generate_page import generate_page
+import os
 import logging
 logger = logging.getLogger(__name__)
 
@@ -10,21 +9,16 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(filename='debug.log', level=logging.INFO)
     logger.info('-- start --')
-    # src = "static"
-    # if os.path.exists(src):
-    #     content = os.listdir(src)
-    #     print(content)
-    #     for c in content:
-    #         full_p = os.path.join(src, c)
-    #         #print(full_p)
-    #         if os.path.isdir(full_p):
-    #             d = os.listdir(full_p)
-    #             for e in d:
-    #                 print(os.path.join(full_p, e))
     logger.info('running update_public_dir()')
     update_public_dir()
     logger.info('-- end --')
+
+    logger.info('Generating pages')
     generate_page('content/index.md', 'template.html', 'public/index.html')
+    generate_page('content/blog/glorfindel/index.md', 'template.html', 'public/blog/glorfindel/index.html')
+    generate_page('content/blog/tom/index.md', 'template.html', 'public/blog/tom/index.html')
+    generate_page('content/blog/majesty/index.md', 'template.html', 'public/blog/majesty/index.html')
+    generate_page('content/contact/index.md', 'template.html', 'public/contact/index.html')
 
 
 if __name__ == "__main__":
